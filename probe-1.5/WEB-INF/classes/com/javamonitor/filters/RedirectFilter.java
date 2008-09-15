90d76de51715f8b5ed122b7341fe0b9f86e8ff55
kjkoster <kjkoster@28b9ff30-831d-11dd-9899-c7ba65f7c4c7>
2008-09-15 12:06:29 +0000

    Initial import.

---
+++ probe-1.5/WEB-INF/classes/com/javamonitor/filters/RedirectFilter.java
@@ -0,0 +1,42 @@
+package com.javamonitor.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * A simple filter that redirects all traffic.
+ * 
+ * @author Kees Jan Koster &lt;kjkoster@kjkoster.org&gt;
+ */
+public class RedirectFilter implements Filter {
+    /**
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    public void init(final FilterConfig config) {
+        // nothing to do...
+    }
+
+    /**
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+     *      javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    public void doFilter(final ServletRequest request,
+            final ServletResponse response, final FilterChain chain)
+            throws IOException {
+        ((HttpServletResponse) response)
+                .sendRedirect("http://www.java-monitor.com/");
+    }
+
+    /**
+     * @see javax.servlet.Filter#destroy()
+     */
+    public void destroy() {
+        // nothing to do...
+    }
+}
