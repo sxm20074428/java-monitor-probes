ff0396b3cd73bfbe15f652a2ec5e41494477d8a4
goltharnl <goltharnl@28b9ff30-831d-11dd-9899-c7ba65f7c4c7>
2008-09-16 17:24:07 +0000

    Work in progress, set SSL if system property is set + infrastructure for loading configuration. More stuff incoming and yes I'll clean up the code tonight ;-)

---
+++ trunk/probe-1.5/WEB-INF/classes/com/javamonitor/SystemProbeConfiguration.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.javamonitor;
+
+/**
+ *
+ * @author barry
+ */
+public class SystemProbeConfiguration implements ProbeConfiguration {
+
+    public String getProperty(String key)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean isEnabled(String key)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
