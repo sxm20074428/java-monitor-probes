9eadf88c982a6a527978fafcee0c4519e2059023
goltharnl <goltharnl@28b9ff30-831d-11dd-9899-c7ba65f7c4c7>
2009-02-21 13:27:12 +0000

    Made changes to allow the probe to be used in non web apps. See Launcher.java for an example. Note, you can use the same WAR (just rename to JAR) as the Servlet classes are not loaded until called

--- trunk/probe-1.5/WEB-INF/classes/com/javamonitor/CollectorServlet.java
+++ trunk/probe-1.5/WEB-INF/classes/com/javamonitor/CollectorServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServlet;
 public class CollectorServlet extends HttpServlet {
     private static final long serialVersionUID = 7230361089078209652L;
 
-    private static final long MINUTES = 60L * 1000L;
+    
 
     private static final Logger log = Logger.getLogger(CollectorServlet.class
             .getName());
@@ -28,17 +28,20 @@ public class CollectorServlet extends HttpServlet {
      */
     @Override
     public void init() throws ServletException {
-        super.init();
 
+        final URL url;
         try {
-            JmxHelper.registerCoolMBeans();
-        } catch (Exception e) {
-            throw new ServletException(e);
+            url = new URL(getServletConfig().getInitParameter("url"));
+        } catch (MalformedURLException e) {
+            log.log(Level.SEVERE,
+                    "Java-monitor collector cannot start: bad url in web.xml: " + e.getMessage(), e);
+            return; // bail out, this is going nowhere
         }
 
-        collectorThread = new Thread(new CollectorDriver(),
-                "java-monitor collector");
-        collectorThread.start();
+
+        super.init();
+
+       
     }
 
     /**
@@ -46,68 +49,8 @@ public class CollectorServlet extends HttpServlet {
      */
     @Override
     public void destroy() {
-        collectorThread.interrupt();
-        try {
-            collectorThread.join();
-        } catch (InterruptedException e) {
-            // ignore, we're going down anyway
-        }
-
-        JmxHelper.unregisterCoolMBeans();
-
         super.destroy();
     }
 
-    private final class CollectorDriver implements Runnable {
-        /**
-         * @see java.lang.Runnable#run()
-         */
-        public void run() {
-            try {
-                final URL url;
-                try {
-                    url = new URL(getServletConfig().getInitParameter("url"));
-                } catch (MalformedURLException e) {
-                    log.log(Level.SEVERE,
-                            "Java-monitor collector cannot start: bad url in web.xml: "
-                                    + e.getMessage(), e);
-                    return; // bail out, this is going nowhere
-                }
-
-                // give the container around us a little time to start up and
-                // (more importantly) register its mbeans.
-                Thread.sleep(2000L);
-
-                for (;;) {
-                    try {
-                        for (;;) {
-                            if (Collector.push(url)) {
-                                Collector.push(url);
-                            }
-
-                            Thread.sleep(1L * MINUTES);
-                        }
-                    } catch (InterruptedException e) {
-                        throw e; // it ends up in the outer try block
-                    } catch (OnHoldException e) {
-                        throw e; // it ends up in the outer try block
-                    } catch (Throwable e) {
-                        log
-                                .log(
-                                        Level.SEVERE,
-                                        "This probe was hit by an unexpected exception (it will retry in fifteen minutes): "
-                                                + e.getMessage(), e);
-                        Thread.sleep(15 * MINUTES);
-                        log.log(Level.INFO, "resuming operation");
-                    }
-                }
-            } catch (InterruptedException e) {
-                // ignore. we're exiting
-            } catch (OnHoldException e) {
-                log.log(Level.SEVERE,
-                        "This probe was put on hold by the collector (redeploy to try again): "
-                                + e.getOnHoldBecause());
-            }
-        }
-    }
+    
 }
