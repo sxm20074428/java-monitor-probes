bae7cfe854bf9d305a52c29391678af99f22a2e2
kjkoster <kjkoster@28b9ff30-831d-11dd-9899-c7ba65f7c4c7>
2009-08-21 08:18:43 +0000

    Change port detection strategy.

--- trunk/probe-1.5/WEB-INF/classes/com/javamonitor/openfire/mbeans/Openfire.java
+++ trunk/probe-1.5/WEB-INF/classes/com/javamonitor/openfire/mbeans/Openfire.java
@@ -17,16 +17,43 @@ public class Openfire implements OpenfireMBean {
                 .getVersionString();
     }
 
+    private static final Integer XMPP_CLIENT_PORT = 5222;
+
     /**
      * @see com.javamonitor.openfire.mbeans.OpenfireMBean#getLowestPort()
      */
     public Integer getLowestPort() {
+        int slept = 0;
+        Integer lowestPort = null;
+        do {
+            lowestPort = findLowestPort();
+            if (XMPP_CLIENT_PORT.equals(lowestPort)) {
+                // this is extremely likely to be correct, so we are done
+                return lowestPort;
+            }
+
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                // won't happen...
+            }
+        } while (slept++ < 30);
+
+        return lowestPort;
+    }
+
+    /**
+     * Find the lowest port number, as the server stands now.
+     * 
+     * @return The lowest known port number, or <code>null</code> if no ports
+     *         were opened yet.
+     */
+    private Integer findLowestPort() {
         int lowestClientPort = Integer.MAX_VALUE;
         int lowestPort = Integer.MAX_VALUE;
         for (final ServerPort port : XMPPServer.getInstance()
                 .getConnectionManager().getPorts()) {
             final int number = port.getPort();
-
             if (number < lowestPort) {
                 lowestPort = number;
             }
